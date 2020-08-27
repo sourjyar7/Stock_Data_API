f9ad79640f899533e933a07dc0fced29e6336a9f
sourjyar7 <sourjyar7@gmail.com>
2020-08-28 03:35:33 +0530

    fixed a few bugs

--- backend/routes/Signup/Signup_routes.js
+++ backend/routes/Signup/Signup_routes.js
@@ -4,14 +4,21 @@ const User=require('../../models/users');
 
 router.post('/',(req,resp)=>{
     if(req.body.username != null && req.body.password != null){
-        const user=new User(req.body);
-        user.save((err,user)=>{
-           if(err) 
-             resp.send({msg: "error"})
-           else
-             resp.send({msg: "signedUp"}); 
-        });
-    }
+       
+        
+            const user=new User(req.body);
+            user.save((err,user)=>{
+               if(err) 
+                 resp.send({msg: "error"})
+               else
+                 resp.send({msg: "signedUp"}); 
+            });
+         
+          
+       
+        }
+        
+    
     else
       resp.send({msg:"error"});   
        
